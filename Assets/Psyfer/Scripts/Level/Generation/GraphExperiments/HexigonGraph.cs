f02243b012eaaf268d0875cff491019edd71f8d3
Oliver Davies <crazevr@gmail.com>
2019-11-05 23:22:30 -0800

    Project cleanup in preparation for reinvigoration.

---
+++ Assets/Psyfer/Scripts/Level/Generation/GraphExperiments/HexigonGraph.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using Random = UnityEngine.Random;
+
+public class HexigonGraph : MonoBehaviour
+{
+
+    public int generationDepth = 10;
+    public int width = 3;
+    public int start = 10;
+
+    private void Start ()
+    {
+        GenerateGraph();
+    }
+
+    private void GenerateGraph()
+    {
+        int depth = 0;
+        NodeLayer current = null;
+        while (depth < generationDepth)
+        {
+            current = new NodeLayer(current, width, depth);
+
+            if (current.prev != null)
+            {
+
+                for (int target = 0; target < width; target++)
+                {
+                    List<int> legal = current.FindPossibleEdges(target);
+                    if(legal.Count > 0) current.ConnectNodes(target, legal[Random.Range(0, legal.Count)]);
+                }
+            
+                //current.TrimOverlappingEdges();
+                //current.TrimDoubleDiagonals();
+
+                for (int i = 0; i < current.prev.nodes.Length; i++)
+                {
+                    Node node = current.prev.nodes[i];
+                    node.Active = node.isConnected;
+                }
+            }
+            else
+            {
+                current.SetNodeActive(start);
+            }
+
+
+            for (int i = 0; i < current.nodes.Length; i++)
+            {
+                current.nodes[i].Show();
+            }
+
+            current.Show();
+            depth++;
+        }
+    }
+}
+
+public class NodeLayer
+{
+    public NodeLayer prev;
+    public Node[] nodes;
+
+    public NodeLayer(NodeLayer prev, int width, int depth)
+    {
+        this.prev = prev;
+
+        // Initialize nodes
+        nodes = new Node[width];
+        for (int i = 0; i < width; i++)
+        {
+            nodes[i] = new Node(prev, i, depth);
+        }
+    }
+
+    public void ConnectNodes(int connectingFrom, int connectingTo)
+    {
+        Node from = nodes[connectingFrom];
+        int localSpaceConnectingTo = from.ToLocalEdgeSpace(connectingTo);
+
+        from.edge[localSpaceConnectingTo] = true;
+
+        SetNodeActive(connectingFrom);
+        SetNodeConnected(connectingTo);
+    }
+
+    public List<int> FindPossibleEdges(int self)
+    {
+        List<int> edges = new List<int>(3);
+        for (int i = 0; i < prev.nodes.Length; i++)
+        {
+            if (Math.Abs(i - self) < 2 && prev.nodes[i].Active)
+            {
+                edges.Add(i);
+            }
+        }
+
+        return edges;
+    }
+
+    public void SetNodeActive(int val)
+    {
+        nodes[val].Active = true;
+    }
+
+    public void SetNodeConnected(int val)
+    {
+        prev.nodes[val].isConnected = true;
+    }
+
+    public void TrimOverlappingEdges()
+    {
+        for (int i = 1; i < nodes.Length - 1; i++)
+        {
+            bool leftCross = nodes[i].edge[0] && nodes[i - 1].edge[2];
+            bool rightCross = nodes[i].edge[2] && nodes[i + 1].edge[0];
+            if(leftCross) nodes[i].edge[0] = false;
+            if(rightCross) nodes[i].edge[2] = false;
+        }
+    }
+
+    public void TrimDoubleDiagonals()
+    {
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            if ((nodes[i].edge[0] || nodes[i].edge[2]) && prev.nodes[i].Active) nodes[i].Active = false;
+        }
+    }
+
+    public void Show()
+    {
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            Debug.DrawRay(Vector3.forward * nodes[i].depth + Vector3.right * i, Vector3.up * 0.1f, nodes[i].Active ? Color.green : Color.red, 10000);
+        }
+    }
+}
+
+public class Node
+{
+    public NodeLayer prev;
+
+    public bool[] edge;
+    public int depth;
+    public int weight;
+    public int position;
+    public bool isConnected;
+
+    private bool active;
+    public bool Active
+    {
+        get { return active; }
+        set
+        {
+            active = value;
+            if (active == false)
+            {
+                for (int index = 0; index < edge.Length; index++)
+                {
+                    edge[index] = false;
+                }
+            }
+        }
+    }
+
+
+    public Node(NodeLayer prev, int position, int depth)
+    {
+        this.prev = prev;
+        this.position = position;
+        this.depth = depth;
+
+        this.active = false;
+        this.isConnected = false;
+        this.edge = new bool[]{false, false, false};
+        this.weight = 0;
+    }
+
+    public bool BoundsCheck(int index)
+    {
+        return Math.Abs(index - position) < 2 && index >= 0 && index < prev.nodes.Length;
+    }
+
+    public int ToLocalEdgeSpace(int p)
+    {
+        return (p - position) + 1;
+    }
+
+    public int ToGlobalEdgeSpace(int index)
+    {
+        return index + (position - 1);
+    }
+
+    public void Show()
+    {
+        if (prev != null)
+        {
+            for (int index = 0; index < edge.Length; index++)
+            {
+                if (edge[index])
+                {
+                    Debug.DrawLine(Vector3.forward * depth + Vector3.right * position, Vector3.forward * prev.nodes[index].depth + Vector3.right * prev.nodes[ToGlobalEdgeSpace(index)].position, Color.cyan, 10000);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Psyfer/Scripts/Level/Generation/PathGenerator.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PathGenerator : MonoBehaviour
+{
+    public GameObject[] tiles;
+
+    private static readonly float HexigonWidthLong = 40;
+    private static readonly float HexigonWidthShort = HexigonWidthLong * 0.5f * Mathf.Sqrt(3f);
+
+    private int modelAngleFix = -60;
+
+    private readonly Dictionary<int, Hexigon.Layout> layoutRequirements = new Dictionary<int, Hexigon.Layout>();
+
+
+    private void Start ()
+    {
+        StartCoroutine(GenerateTree());
+    }
+
+    public bool WaitTillSpace()
+    {
+        return Input.GetKeyDown(KeyCode.Space);
+    }
+
+    public IEnumerator GenerateTree()
+    {
+        // Variables that change per tile
+        int tileDepth = 0;
+
+        Stack<Hexigon> currentHexigonDepth = new Stack<Hexigon>();
+        Stack<Hexigon> nextHexigonDepth = new Stack<Hexigon>();
+
+        Hexigon root = new Hexigon(Hexigon.Layouts[0], PositionToHash(Vector3.zero), Vector3.zero, tileDepth);
+        currentHexigonDepth.Push(root);
+        root = ApplyLayoutConstraints(root);
+        SpawnHexigonWorldObject(root);
+        tileDepth++;
+
+        while (tileDepth < 10)
+        {
+            Hexigon cur = currentHexigonDepth.Pop();
+            for (int i = 0; i < cur.layout.connections.Length; i++)
+            {
+                Hexigon.ConnectorType connectionType = cur.layout.connections[i];
+                if (!CheckLegalOutput(i, connectionType)) continue;
+
+                Vector3 worldPosition = GenerateHexigonWorldPosition(cur, i);
+                int hash = PositionToHash(worldPosition, true);
+                Debug.Assert(cur.Hash != hash, "Current hash is equal to new tile hash!");
+                Hexigon next = new Hexigon(ChooseLegalLayout(hash, worldPosition), hash, worldPosition, tileDepth);
+                next = ApplyLayoutConstraints(next);
+
+                SpawnHexigonWorldObject(next);
+                nextHexigonDepth.Push(next);
+                yield return new WaitForEndOfFrame();
+            }
+
+            // Swap stacks
+            if (currentHexigonDepth.Count < 1)
+            {
+                Stack<Hexigon> swapReference = currentHexigonDepth;
+                currentHexigonDepth = nextHexigonDepth;
+                nextHexigonDepth = swapReference;
+                tileDepth++;
+            }
+        }
+    }
+
+    protected Hexigon ApplyLayoutConstraints(Hexigon hexigon)
+    {
+        hexigon.layout.isFringe = false;
+        layoutRequirements[hexigon.Hash] = hexigon.layout;
+
+        // Surrounding tiles need rules
+        for (int i = 0; i < hexigon.layout.connections.Length; i++)
+        {
+            Hexigon.ConnectorType connectionType = hexigon.layout.connections[i];
+            if (!CheckLegalOutput(i, connectionType)) continue;
+
+            Vector3 worldPosition = GenerateHexigonWorldPosition(hexigon, i);
+            int hash = PositionToHash(worldPosition);
+
+            if (!layoutRequirements.ContainsKey(hash))
+            {
+                layoutRequirements[hash] = new Hexigon.Layout(-1, new[] {0, 0, 0, 0, 0, 0});
+            }
+
+            Hexigon.Layout result = layoutRequirements[hash];
+            if (result.isFringe)
+            {
+                result.AddConnection((i + 3) % 6, Hexigon.ConnectorType.INPUT);
+                layoutRequirements[hash] = result;
+            }
+
+            Debug.DrawLine(hexigon.WorldPosition, worldPosition, Color.red, 100);
+            Debug.DrawRay(worldPosition, Vector3.up, Color.green, 100);
+            Debug.DrawRay(worldPosition, Vector3.up * 50, Color.white, 5);
+        }
+
+        return hexigon;
+    }
+
+    protected bool CheckLegalOutput(int direction, Hexigon.ConnectorType type)
+    {
+        return type == Hexigon.ConnectorType.OUTPUT;
+    }
+
+    protected Hexigon.Layout ChooseLegalLayout(int hash, Vector3 worldPosition)
+    {
+        Hexigon.Layout target = layoutRequirements[hash];
+        List<Hexigon.Layout> layouts = target.FindMatchingLayouts();
+        for (int i = 0; i < layouts.Count; i++)
+        {
+            Hexigon.Layout layout = layouts[i];
+            foreach (Hexigon.ConnectorType connection in layout.connections)
+            {
+                if (connection == Hexigon.ConnectorType.OUTPUT)
+                {
+                    int h = PositionToHash(worldPosition, (int) connection);
+                    if (layoutRequirements.ContainsKey(h))
+                    {
+                        if (!layoutRequirements[h].isFringe)
+                        {
+                            layouts.RemoveAt(i);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return layouts[Random.Range(0, layouts.Count)];
+    }
+
+    protected Vector3 GenerateHexigonWorldPosition(Vector3 position, int direction)
+    {
+        return position + Quaternion.AngleAxis((60 * direction) + modelAngleFix, Vector3.up) * Vector3.forward * HexigonWidthShort;
+    }
+
+    protected Vector3 GenerateHexigonWorldPosition(Hexigon hexigon, int direction)
+    {
+        return hexigon.WorldPosition + Quaternion.AngleAxis((60 * direction) + modelAngleFix, Vector3.up) * Vector3.forward * HexigonWidthShort;
+    }
+
+    protected void SpawnHexigonWorldObject(Hexigon hexigon)
+    {
+        Debug.DrawRay(hexigon.WorldPosition, Vector3.up * 50, Color.yellow, 5);
+        if (hexigon.layout.primitiveLayoutType < 0)
+        {
+            Debug.LogError("Tile type not found");
+            return;
+        }
+        GameObject hexi = Instantiate(tiles[hexigon.layout.primitiveLayoutType], hexigon.WorldPosition + Vector3.up * hexigon.Depth * HexigonWidthLong * 0.1f, Quaternion.AngleAxis((60 * -hexigon.layout.rotationOffset) + 30, Vector3.up));
+    }
+
+    protected int PositionToHash(Vector3 position, bool print=false)
+    {
+        Vector2Int result = new Vector2Int(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.z));
+        int hash = result.GetHashCode();
+
+        if (print) Debug.Log(hash + ", " + result + ", " + position);
+        return hash;
+    }
+
+    protected int PositionToHash(Hexigon parentHexigon, int direction)
+    {
+        Vector3 position = GenerateHexigonWorldPosition(parentHexigon, direction);
+        return new Vector2Int(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.z)).GetHashCode();
+    }
+
+    protected int PositionToHash(Vector3 p, int direction)
+    {
+        Vector3 position = GenerateHexigonWorldPosition(p, direction);
+        return new Vector2Int(Mathf.RoundToInt(position.x), Mathf.RoundToInt(position.z)).GetHashCode();
+    }
+}
